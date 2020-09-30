3bc9d44d626aecb5e1b56d369a787f93362c6252
SavrBd <savr.bd@yandex.ru>
2020-09-30 22:30:10 +0300

    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+if ( array===undefined||array.length===0) {return 0}
+else{
+  let min=0;
+  for(let i=0;i<array.length; i++) {
+    if (array[i]<min) {min=array[i];}
+  }
+return min;
+}
+  
 }
 
+
+
+
 exports.max = function max (array) {
-  return 0;
+ if ( array===undefined||array.length===0) {return 0}
+else {
+  let max=0;
+  for (let j=0; j<array.length; j++) {
+    if (array[j]>max) {max=array[j];}
+  }
+  return max;
+}
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+ 
+  if ( array===undefined||array.length===0) {return 0}
+  else {
+    let result=0
+    for (k=0; k< array.length; k++){
+      result=result +array[k];
+    }
+    return result/ array.length;
+  } 
 }
